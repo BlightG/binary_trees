98738d70e9ead90c66b4f37d17c0cfc7f83fe0a3
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-13 19:41:34 +0300

    freeing a binary tree

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * 
+*/
+void binary_tree_delete(binary_tree_t *tree)
+{
+	/**
+	 * check if @tree is a leaf
+	 * if not go to left node check if it is a leaf
+	 * if it is clear left node 
+	 * check right node after finishing left node subbranches
+	 * after finishing with right node free tree
+	*/
+
+        if (!tree)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
