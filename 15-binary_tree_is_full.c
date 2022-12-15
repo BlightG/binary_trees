0fa080762cd5673525cb05fa049a62457cb6e327
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-15 11:45:09 +0300

    initalized left nad right count as 1 in task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,7 +8,7 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-        int left_check, right_check;
+        int left_check = 1, right_check = 1;
         /* if tree is NULL return 0 */
         if (!tree)
                 return (0);
