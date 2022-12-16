b7aa5c3c8a043e4c3f9a95ccdfa272bd286f3dd4
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-12-16 19:15:00 +0300

    made the binary_trees_ancestor function shorter

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -20,32 +20,28 @@ size_t bin_tree_depth(const binary_tree_t *tree)
 
 	return (tree_depth);
 }
+
 /**
- * binary_trees_ancestor - a funciton to find the least 
- * 			   common ancestor of two nodes
- * @first: first node to compare
- * @second: second node to compare
- * 
- * Return: returns a common ancestor or null
+ * match_depth_diff - a function to makethe depth b/n two nodes is the same
+ *
+ * @temp_1: first node to have depth adjusted
+ * @temp_2: second node to have depth adjusted
 */
-binary_tree_t *binary_trees_ancestor(
-	const binary_tree_t *first, const binary_tree_t *second)
+void match_depth_diff(binary_tree_t **, binary_tree_t **);
+void match_depth_diff(binary_tree_t **first, binary_tree_t **second)
 {
 	binary_tree_t *temp_1, *temp_2;
 	size_t first_depth, second_depth;
 	int depth_diff, i = 0;
-	/* check first and seocnd are not NULL*/
-	if (!first || !second)
-		return (NULL);
 
-	temp_1 = (binary_tree_t *) first;
-	temp_2 = (binary_tree_t *) second;
-	first_depth = (int) bin_tree_depth(first);
-	second_depth = (int) bin_tree_depth(second);
+	temp_1 = *first;
+	temp_2 = *second;
+	first_depth = (int) bin_tree_depth(temp_1);
+	second_depth = (int) bin_tree_depth(temp_2);
 	depth_diff = first_depth - second_depth;
 
-	/* if the depth_diff is postive move temp_1 back until both are same level*/
-	/* if it is negative do the opposite*/
+	/* if the depth_diff is postive move temp_1 back until both are same level */
+	/* if it is negative do the opposite */
 	if (depth_diff > 0)
 	{
 		i = depth_diff;
@@ -64,14 +60,42 @@ binary_tree_t *binary_trees_ancestor(
 			i--;
 		}
 	}
+
+	/* set the pointer at the inital adress to the new adress */
+	*first = temp_1;
+	*second = temp_2;
+}
+/**
+ * binary_trees_ancestor - a funciton to find the least
+ * 			   common ancestor of two nodes
+ * @first: first node to compare
+ * @second: second node to compare
+ *
+ * Return: returns a common ancestor or null
+*/
+binary_tree_t *binary_trees_ancestor(
+	const binary_tree_t *first, const binary_tree_t *second)
+{
+	binary_tree_t *temp_1, *temp_2;
+
+	/* check first and seocnd are not NULL*/
+	if (!first || !second)
+		return (NULL);
+
+	temp_1 = (binary_tree_t *) first;
+	temp_2 = (binary_tree_t *) second;
+
+	/* change the depth diff if it exists */
+	match_depth_diff(&temp_1, &temp_2);
+
 	/* check if first and second are same node */
 	/* else check if they belong to the same parent*/
 	/* else do a loop backwards in the tree */
-	if (first == second)
+	if (temp_1 == temp_2)
 		return (temp_1);
-	else if (first->parent == second->parent)
-		return (first->parent);
-	
+	else if (temp_1->parent == temp_2->parent)
+		return (temp_1->parent);
+
 	while (temp_1->parent && temp_2->parent)
 	{
 		/* when you find a mathcing node stop looping back*/
